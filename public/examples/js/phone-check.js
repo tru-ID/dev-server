65c978a10888741a436ef471910a505e737e2fac
Phil Leggetter <phil@leggetter.co.uk>
2021-01-28 21:16:17 +0000

    feat: handle Chrome on Android by opening new window

--- public/examples/js/phone-check.js
+++ public/examples/js/phone-check.js
@@ -7,19 +7,31 @@ window.tru.ID.phoneCheck = function(checkUrl) {
     }
 
     return new Promise(function(resolve) {
-        const img = new Image()
-        img.style.height = 0
-        img.style.width = 0
-        img.setAttribute('referrerpolicy', 'no-referrer')
-        img.src = checkUrl
-    
-        function handleEndEvent() {
-            document.body.removeChild(img)
-            resolve()
+        const userAgent = window.navigator.userAgent
+        if( userAgent.toLowerCase().indexOf('Android') !== 0 &&
+            userAgent.toLowerCase().indexOf('Chrome') !== 0) {
+            // Chrome on Android
+            const win = window.open(checkUrl)
+            setTimeout(() => {
+                win.close()
+                resolve()
+            }, 2000)
         }
-    
-        img.onload = handleEndEvent
-        img.onerror = handleEndEvent
-        document.body.appendChild(img)
+        else {
+            const img = new Image()
+            img.style.height = 0
+            img.style.width = 0
+            img.setAttribute('referrerpolicy', 'no-referrer')
+            img.src = checkUrl
+        
+            function handleEndEvent() {
+                document.body.removeChild(img)
+                resolve()
+            }
+        
+            img.onload = handleEndEvent
+            img.onerror = handleEndEvent
+            document.body.appendChild(img)
+        }
     })
 }
