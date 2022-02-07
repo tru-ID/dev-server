e53983663d689729f574473dbe5dc00c4c45833e
Greg Holmes <greg.holmes@tru.id>
2022-02-07 17:44:31 +0000

    Updated config.js to correctly retrieve only ngrok information relevant

--- src/config.js
+++ src/config.js
@@ -11,7 +11,7 @@ const DEBUG =
 const API_BASE_URL = process.env.API_BASE_URL ?? 'https://eu.api.tru.id'
 const CLIENT_ID = process.env.TRU_ID_CLIENT_ID
 const CLIENT_SECRET = process.env.TRU_ID_CLIENT_SECRET
-const { PROJECT_PATH } = process.env.PROJECT_PATH
+const { PROJECT_PATH } = process.env
 const LOCALTUNNEL_ENABLED = process.env.LOCALTUNNEL_ENABLED
   ? process.env.LOCALTUNNEL_ENABLED === 'true'
   : false
@@ -20,7 +20,6 @@ const LOCALTUNNEL_SUBDOMAIN = process.env
 const NGROK_ENABLED = process.env.NGROK_ENABLED
   ? process.env.NGROK_ENABLED === 'true'
   : false
-const { NGROK_SUBDOMAIN, NGROK_AUTHTOKEN, NGROK_REGION } = process.env
 
 async function configure(params) {
   const processConfig = {
@@ -37,9 +36,6 @@ async function configure(params) {
     },
     ngrok: {
       enabled: NGROK_ENABLED,
-      subdomain: NGROK_SUBDOMAIN,
-      authtoken: NGROK_AUTHTOKEN,
-      region: NGROK_REGION,
     },
     log: (...args) => {
       if (DEBUG) {
